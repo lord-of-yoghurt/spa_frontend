68e2e9907cb45b14aae9d09c397d049eeb797965
David Ordovskiy <david.ordovsky@gmail.com>
2017-08-14 11:40:06 -0400

    try to add sending post form data via fetch, broken for now

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import 'whatwg-fetch'
 
 export default class PostForm extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      title: 'Title here',
-      body: 'So what happened?',
-      username: 'Your nickname plz'
+      title: '',
+      body: '',
+      username: ''
     }
 
     this.handleChange     = this.handleChange.bind(this);
@@ -31,13 +32,25 @@ export default class PostForm extends React.Component {
       title: '',
       body: '',
       username: ''
-    })
+    }, () => console.log('yay!'))
   }
 
   handleSubmit(event) {
     event.preventDefault()
+    const payload = this.state
+    fetch('http://localhost:3000/api/posts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(res => {
+      return res.json()
+    })
+    .then(data => console.log(JSON.stringify(data)))
     this.handleClearForm(event)
-    console.log(this.state)
   }
 
   render() {
@@ -46,11 +59,10 @@ export default class PostForm extends React.Component {
         <hr></hr>
         <h3>New Post</h3>
         <form onSubmit={this.handleSubmit}>
-          <p><input name="title" type="text" placeholder={this.state.title} onChange={this.handleChange} /></p>
-          <p><textarea name="body" placeholder={this.state.body} onChange={this.handleChange} /></p>
-          <p><input name="username" type="text" placeholder={this.state.username} onChange={this.handleChange} /></p>
+          <p>Title: <input name="title" type="text" value={this.state.title} onChange={this.handleChange} /></p>
+          <p>Body: <textarea name="body" value={this.state.body} placeholder={this.state.body} onChange={this.handleChange} /></p>
+          <p>Username: <input name="username" type="text" value={this.state.username} placeholder={this.state.username} onChange={this.handleChange} /></p>
           <p><input type="submit" value="Yarrr!" /></p>
-          <p><button onClick={this.handleClearForm}>Clear this mess</button></p>
         </form>
       </div>
     )
